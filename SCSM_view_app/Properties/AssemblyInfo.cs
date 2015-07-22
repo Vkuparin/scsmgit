d5bc378323157fc660b099fc86f1523066b0a5a7
Vkuparin <villekuparinen@hotmail.com>
2015-07-22 16:30:10 +0300

    Edistystä jee

--- SCSM_view_app/Properties/AssemblyInfo.cs
+++ SCSM_view_app/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("show_inident")]
+[assembly: AssemblyTitle("show_incident")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Turun kaupunki")]
-[assembly: AssemblyProduct("show_inident")]
+[assembly: AssemblyProduct("show_incident")]
 [assembly: AssemblyCopyright("Copyright © Turun kaupunki 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- SCSM_view_app/TicketFill.aspx.cs
+++ SCSM_view_app/TicketFill.aspx.cs
@@ -11,16 +11,36 @@ using System.Configuration;
 using System.Web.Services;
 using System.Web.Script.Services;
 using System.Diagnostics;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.DirectoryServices.AccountManagement;
 
-public partial class TicketFill : System.Web.UI.Page {
+public partial class TicketFill : System.Web.UI.Page
+{
+        private string _userAccountName = Environment.UserName;
+        protected string userAccountName { get { return this._userAccountName; } }
         
         protected void Page_Load(object sender, EventArgs e)
         {
-            
-          
+
+            PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
+
+            // define a "query-by-example" principal - here, we search for a UserPrincipal 
+            UserPrincipal qbeUser = new UserPrincipal(ctx);
+
+            // if you're looking for a particular user - you can limit the search by specifying
+            // e.g. a SAMAccountName, a first name - whatever criteria you are looking for
+            qbeUser.SamAccountName = userAccountName;
+
+            // create your principal searcher passing in the QBE principal    
+            PrincipalSearcher srch = new PrincipalSearcher(qbeUser);
+
+            // find all matches
+            foreach (var found in srch.FindAll())
+            {
+                    
+            }
+
         }
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            Label1.Text = "Clicked at " + DateTime.Now.ToString();
-        }
+
 }
