dca362dde00881fa2434b7ecd153ec809f71e0bb
Vkuparin <villekuparinen@hotmail.com>
2015-07-23 16:46:39 +0300

    databeissii

--- SCSM_view_app/TicketFill.aspx.cs
+++ SCSM_view_app/TicketFill.aspx.cs
@@ -24,11 +24,20 @@ public partial class TicketFill : System.Web.UI.Page
         private string _computerName = Environment.MachineName;
         protected string computerName { get { return this._computerName; } }
 
+        private string _testi = "";
+        protected string testi { get { return this._testi; } }
+
         private string _osName = getOsName() + ", Käyttöjärjestelmän versio: "+Environment.OSVersion.ToString();
         protected string osName { get { return this._osName; } }
 
-        private string _testi = "";
-        protected string testi { get { return this._testi; } }
+        private string[] _userInfoAD = null;
+        protected string userFullName { get { return this._userInfoAD[0]; }}
+        protected string userEmail { get { return this._userInfoAD[1]; } }
+        protected string userPhone { get { return this._userInfoAD[2]; } }
+        protected string userCompany { get { return this._userInfoAD[3]; } }
+        protected string userDepartment { get { return this._userInfoAD[4]; } }
+        protected string userOffice { get { return this._userInfoAD[5]; } }
+        protected string userCity { get { return this._userInfoAD[6]; } }
 
         private static string getOsName()
         {
@@ -39,43 +48,103 @@ public partial class TicketFill : System.Web.UI.Page
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
-
-            // define a "query-by-example" principal - here, we search for a UserPrincipal 
-            UserPrincipal qbeUser = new UserPrincipal(ctx);
-
-            // if you're looking for a particular user - you can limit the search by specifying
-            // e.g. a SAMAccountName, a first name - whatever criteria you are looking for
-            qbeUser.SamAccountName = userAccountName;
-
-            // create your principal searcher passing in the QBE principal    
-            PrincipalSearcher srch = new PrincipalSearcher(qbeUser);
-
-            // find all matches
-            foreach (var found in srch.FindAll())
+            using (DirectoryEntry de = new DirectoryEntry("LDAP://adturku.fi"))
             {
-                    
+                using (DirectorySearcher adSearch = new DirectorySearcher(de))
+                {
+                    adSearch.PropertiesToLoad.Add("cn");  // Kokonimi
+                    adSearch.PropertiesToLoad.Add("mail");  // Sähköposti
+                    //adSearch.PropertiesToLoad.Add("telephoneNumber");  // Puhelinnumero
+                    //adSearch.PropertiesToLoad.Add("Company");  // Toimiala
+                    //adSearch.PropertiesToLoad.Add("Department");  // Yksikkö
+                    //adSearch.PropertiesToLoad.Add("office");  // Toimipiste
+                    //adSearch.PropertiesToLoad.Add("City");  // kaupunki
+                    adSearch.Filter = "(sAMAccountName=slahdeti)"; //+ userAccountName+
+                    SearchResult adSearchResult = adSearch.FindOne();
+                    var searchPropCollection = adSearchResult.Properties;
+                    string[] info = new string[15];
+                    int infoRivi = 0;
+                    foreach (string tulos in searchPropCollection.PropertyNames)
+                    {
+
+                        _testi += tulos + " | ";
+                        if (tulos.Equals("cn"))
+                        {
+                            infoRivi = 0;
+                        }
+                        if (tulos.Equals("mail"))
+                        {
+                            infoRivi = 1;
+                        }
+                        if (tulos.Equals("telephonenumber"))
+                        {
+                            infoRivi = 2;
+                        }
+                        if (tulos.Equals("company"))
+                        {
+                            infoRivi = 3;
+                        }
+                        if (tulos.Equals("department"))
+                        {
+                            infoRivi = 4;
+                        }
+                        if (tulos.Equals("office"))
+                        {
+                            infoRivi = 5;
+                        }
+                        
+                        foreach (Object myCollection in searchPropCollection[tulos])
+                        {
+                            info[infoRivi] = myCollection.ToString();
+                        }
+                    }
+                    _userInfoAD = info;
+                }
             }
 
             using (DirectoryEntry de = new DirectoryEntry("LDAP://adturku.fi"))
             {
                 using (DirectorySearcher adSearch = new DirectorySearcher(de))
                 {
-                    adSearch.PropertiesToLoad.Add("Name");  // given (or first) name
-                    adSearch.PropertiesToLoad.Add("mail");  // e-mail addresse
-                    adSearch.PropertiesToLoad.Add("telephoneNumber");  // phone number
-                    adSearch.Filter = "(sAMAccountName=" + userAccountName+")";
+                    adSearch.PropertiesToLoad.Add("telephoneNumber");  // Puhelinnumero
+                    adSearch.PropertiesToLoad.Add("Company");  // Toimiala
+                    adSearch.PropertiesToLoad.Add("Department");  // Yksikkö
+                    //adSearch.PropertiesToLoad.Add("Office");  // Toimipiste
+                    //adSearch.PropertiesToLoad.Add("City");  // kaupunki
+                    adSearch.Filter = "(sAMAccountName=slahdeti)"; //+ userAccountName+
                     SearchResult adSearchResult = adSearch.FindOne();
                     var searchPropCollection = adSearchResult.Properties;
+                    string[] info = new string[15];
+                    int infoRivi = 0;
                     foreach (string tulos in searchPropCollection.PropertyNames)
                     {
-                        string tab = "    ";
-                        _testi += tulos + " = ";
+
+
+                        if (tulos.Equals("telephonenumber"))
+                        {
+                            infoRivi = 2;
+                        }
+                        if (tulos.Equals("company"))
+                        {
+                            infoRivi = 3;
+                        }
+                        if (tulos.Equals("department"))
+                        {
+                            infoRivi = 4;
+                        }
+                        if (tulos.Equals("office"))
+                        {
+                            infoRivi = 5;
+                        }
+
                         foreach (Object myCollection in searchPropCollection[tulos])
                         {
-                            _testi += tab + myCollection.ToString();
+                            info[infoRivi] = myCollection.ToString();
                         }
                     }
+                    _userInfoAD[3] = info[3];
+                    _userInfoAD[4] = info[4];
+                    _userInfoAD[5] = info[5];
                 }
             }
 
